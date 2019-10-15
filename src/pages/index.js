c334a515502c5b699d7ffc54f579d5114d754301
= <felix.thalin@live.com>
2019-10-15 16:05:47 +0200

    added branch and started on new start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,26 @@ import Container from "../components/Container"
 
 export default ({ data }) => {
   console.log(data)
+
   return (
     <div>
-      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div key={node.id}>
-          <h3>
-            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-          </h3>
-          <p>{node.excerpt}</p>
-        </div>
-      ))}
+      <h1>Välkommen till GuteTeknik!</h1>
     </div>
   )
+
+  // return (
+  //   <div>
+  //     <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+  //     {data.allMarkdownRemark.edges.map(({ node }) => (
+  //       <div key={node.id}>
+  //         <h3>
+  //           <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+  //         </h3>
+  //         <p>{node.excerpt}</p>
+  //       </div>
+  //     ))}
+  //   </div>
+  // )
 }
 
 export const query = graphql`
