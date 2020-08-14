ed4d192b4b1c6e370729b61f97c7a6669d6d31d8
= <felix.thalin@live.com>
2020-08-14 13:54:29 +0200

    added style to PracticeContainer

--- src/components/mdxComponents/PracticeContainer.js
+++ src/components/mdxComponents/PracticeContainer.js
@@ -1,18 +1,54 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { useState } from "react"
 
 const Hidden = styled.section`
+  font-size: 16px;
+  padding: 0.5rem;
   background: lightgray;
+  animation-duration: 0.3s;
+  animation-name: slidein;
+  border-radius: 0 0.2rem 0.2rem 0.2rem;
+
+  @keyframes slidein {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
 `
 
 const ShowButton = styled.button`
+  font-size: 1rem;
   background: green;
   border: none;
   padding: 0.5rem 1rem;
   border-radius: 0.2rem;
   color: white;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background: darkgreen;
+  }
+
+  ${props =>
+    props.expanded &&
+    css`
+      border-radius: 0.2rem 0.2rem 0 0;
+      background: red;
+      &:hover {
+        background: darkred;
+      }
+    `};
 `
+
+function HiddenDiv({ isHidden, children }) {
+  return <>{!isHidden && <Hidden>{children}</Hidden>}</>
+}
 
 function PracticeContainer({ children }) {
   const show = () => {
@@ -23,14 +59,12 @@ function PracticeContainer({ children }) {
   const [isHidden, setHidden] = useState(true)
 
   return (
-    <div>
-      <ShowButton onClick={() => show()}>Visa lösning</ShowButton>
-      {!isHidden && (
-        <Hidden>
-          <p>PracticeContainer Works! {children}</p>
-        </Hidden>
-      )}
-    </div>
+    <>
+      <ShowButton expanded={!isHidden} onClick={() => show()}>
+        {isHidden ? "Visa" : "Dölj"} lösning
+      </ShowButton>
+      <HiddenDiv isHidden={isHidden}>{children}</HiddenDiv>
+    </>
   )
 }
 
