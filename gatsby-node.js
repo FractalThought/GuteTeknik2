92614554c3c6ec24fae6ddd6fac386e92e3dda55
= <felix.thalin@live.com>
2020-10-12 12:32:58 +0200

    rolled back previous test, not sure what it did but caused build to fail

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,11 +85,3 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 }
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
-    },
-  })
-}
