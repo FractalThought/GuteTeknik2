aef48667768706f787f97eceefc25df3fcc201d0
= <felix.thalin@live.com>
2020-10-21 16:02:26 +0200

    added chapter pages for prog1

---
+++ src/pages/prog1/kontrollsatser.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ChapterPage from "../../layout/ChapterPage"
+
+export default () => {
+  return (
+    <ChapterPage course="prog1" chapter="kontrollsatser">
+      <p>I det här kapitlet introducerar vi JavaScript på webben.</p>
+    </ChapterPage>
+  )
+}
