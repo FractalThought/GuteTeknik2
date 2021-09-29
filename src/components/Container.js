c4ce44c6a96ed059d8f6ae9b12d63987ed3fa77e
FelixGute <felix.thalin@guteskolan.se>
2021-09-29 20:59:51 +0200

    worked on new layout, inspired by tailwind docs

--- src/components/Container.js
+++ src/components/Container.js
@@ -13,11 +13,6 @@ const ClearDiv = styled.div`
   clear: both;
 `;
 
-const ContentContainer = styled.div`
-  margin: 0 auto;
-  max-width: 70ch;
-`;
-
 function Container({
   url,
   pageInfo,
@@ -51,39 +46,45 @@ function Container({
   // TODO: Abstract away the Container so it is the main layout component for all views
 
   return (
-    <div id="main-grid">
+    <div id="main-wrapper">
       <MainContext.Provider value={mainInfo}>
-        <MainLayout>
+        <ContextWrapper>
           <Header
             sidebarUtility={{ showSidebar, setSideBarVisibility }}
             mainPage={mainPage}
           />
-          <Sidebar
-            showSidebar={showSidebar}
-            url={urlData}
-            currentPageData={currentPageData}
-          />
-          <main>
-            <ContentContainer className="page">
-              <MyCrumbs crumbData={crumbData} />
-              <h1 className="page-heading">{pageTitle}</h1>
-              {/* <h1 className="printheader">{pageTitle}</h1> */}
-              <MDXProvider components={mdxComponents}>{children}</MDXProvider>
-              <ClearDiv></ClearDiv>
-            </ContentContainer>
-          </main>
-          <aside className="page-index">
-            <RightStickyDiv>
-              <TableOfContent listOfContent={listOfContent} />
-            </RightStickyDiv>
-          </aside>
-        </MainLayout>
+          <div className="layout-container">
+            <Sidebar
+              showSidebar={showSidebar}
+              url={urlData}
+              currentPageData={currentPageData}
+            />
+            <div className="content-container">
+              <main>
+                <div className="page">
+                  <MyCrumbs crumbData={crumbData} />
+                  <h1 className="page-heading">{pageTitle}</h1>
+                  {/* <h1 className="printheader">{pageTitle}</h1> */}
+                  <MDXProvider components={mdxComponents}>
+                    {children}
+                  </MDXProvider>
+                  <ClearDiv></ClearDiv>
+                </div>
+              </main>
+              <aside className="page-index">
+                <RightStickyDiv>
+                  <TableOfContent listOfContent={listOfContent} />
+                </RightStickyDiv>
+              </aside>
+            </div>
+          </div>
+        </ContextWrapper>
       </MainContext.Provider>
     </div>
   );
 }
 
-const MainLayout = ({ children }) => {
+const ContextWrapper = ({ children }) => {
   const CurrentMainInfo = useContext(MainContext);
   CurrentMainInfo.currentHeadings = [];
 
