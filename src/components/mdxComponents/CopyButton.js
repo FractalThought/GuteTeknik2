3ab82ab9c7302973f13a12b00226f6ea008183c1
FelixGute <felix.thalin@guteskolan.se>
2021-10-06 20:15:42 +0200

    Made the copy button actually copy the code, and made it change state when it does

--- src/components/mdxComponents/CopyButton.js
+++ src/components/mdxComponents/CopyButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
@@ -9,22 +9,40 @@ const Button = styled.button`
 `;
 
 function CopyButton({ children }) {
+  const [isCopied, setCopied] = useState(false);
+
   let theCode = "";
   if (Array.isArray(children)) {
+    // if the code has empty lines, it gets divided to multiple paragraph nodes
     children.forEach(child => {
-      theCode += child.props.children + "\n\n";
+      theCode += child.props.children + "\n\n"; // Add in two break lines to recreated the empty lines between the code lines
     });
   } else {
     theCode = children.props.children;
   }
 
-  console.log(theCode);
   function doCopy() {
     // Code for copying
-    console.log(theCode);
+    navigator.clipboard.writeText(theCode).then(
+      function() {
+        /* clipboard successfully set */
+        // Visa meddelande att den har kopierat koden.
+        setCopied(true);
+        setTimeout(() => {
+          setCopied(false);
+        }, 2000);
+      },
+      function() {
+        /* clipboard write failed */
+      }
+    );
   }
 
-  return <Button onClick={doCopy}>Kopiera koden</Button>;
+  return (
+    <Button onClick={doCopy}>
+      {isCopied ? "Koden kopierad" : "Kopiera koden"}
+    </Button>
+  );
 }
 
 export default CopyButton;
